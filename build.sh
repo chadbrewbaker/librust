448b5dc4b3691d97baa506cde5948cfc70251804
Chad Brewbaker <chad@flyingdogsolutions.com>
2021-05-18 17:47:02 -0500

    Getting hello world building on Linux x86

--- build.sh
+++ build.sh
@@ -1,2 +1,7 @@
-cargo build
-gcc main.c -l v0_baseline -L  target/debug -o hello
+# This was working on aarch64 macos
+#cargo build
+#gcc main.c -l v0_baseline -L  target/debug -o hello
+
+
+RUSTFLAGS="-C target-feature=+crt-static" cargo build --lib --release  
+clang main.c -l v0_baseline -L  target/release -o hello  -lpthread -ldl 
