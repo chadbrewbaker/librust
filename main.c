ec72e371afa792485fdc5225f1c2b6fe552fe6db
Chad Brewbaker <chad@flyingdogsolutions.com>
2021-05-18 19:10:11 -0500

    Got string counter working

--- main.c
+++ main.c
@@ -1,6 +1,28 @@
-extern  void hello();
+#include <inttypes.h>
+#include <stdint.h>
+#include <stdio.h>
+
+typedef struct string_counter string_counter_t;
+
+extern void hello();
+
+extern string_counter_t *string_counter_new(void);
+
+extern void string_counter_free(string_counter_t *);
+
+extern void string_counter_insert(const string_counter_t *, const char *);
+
+extern uint64_t string_counter_count(const string_counter_t *, const char *);
 
 int main() {
-    hello();
-    return 0;
+  uint64_t cnt;
+  hello();
+  string_counter_t *sc = string_counter_new();
+  cnt = string_counter_count(sc, "jabber");
+  printf("%" PRId64 "\n", cnt);
+  string_counter_insert(sc, "jabber");
+  cnt = string_counter_count(sc, "jabber");
+  printf("%" PRId64 "\n", cnt);
+  string_counter_free(sc);
+  return 0;
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,94 @@
+extern crate libc;
+
+use libc::c_char;
+use std::collections::HashMap;
+use std::ffi::CStr;
+
+pub struct StringCounter {
+    elts: HashMap<String, u64>,
+}
+
+impl StringCounter {
+    fn new() -> StringCounter {
+        StringCounter {
+            elts: HashMap::new(),
+        }
+    }
+
+    fn insert(&mut self, key: &str) {
+        match self.elts.get_mut(key) {
+            Some(counter) => *counter = *counter + 1,
+            None => {
+                self.elts.insert(key.to_string(), 1); //{
+                                                      // Some(counter) => ,
+                                                      //None =>  //
+            }
+        }
+    }
+
+    fn count(&self, key: &str) -> u64 {
+        match self.elts.get(key) {
+            Some(counter) => *counter,
+            None => 0,
+        }
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn string_counter_new() -> *mut StringCounter {
+    Box::into_raw(Box::new(StringCounter::new()))
+}
+
+#[no_mangle]
+pub extern "C" fn string_counter_free(ptr: *mut StringCounter) {
+    if ptr.is_null() {
+        return;
+    }
+    unsafe {
+        Box::from_raw(ptr);
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn string_counter_insert(ptr: *mut StringCounter, key: *const c_char) {
+    let sc = unsafe {
+        assert!(!ptr.is_null());
+        &mut *ptr
+    };
+    let key = unsafe {
+        assert!(!key.is_null());
+        CStr::from_ptr(key)
+    };
+    let key_str = key.to_str().unwrap();
+    sc.insert(key_str)
+}
+
+#[no_mangle]
+pub extern "C" fn string_counter_count(ptr: *mut StringCounter, key: *const c_char) -> u64 {
+    let sc = unsafe {
+        assert!(!ptr.is_null());
+        &mut *ptr
+    };
+    let key = unsafe {
+        assert!(!key.is_null());
+        CStr::from_ptr(key)
+    };
+    let key_str = key.to_str().unwrap();
+    sc.count(key_str)
+}
+
+// https://doc.rust-lang.org/reference/type-layout.html
+
+#[repr(C)]
+pub struct Foo {
+    _data: u64,
+    _marker: core::marker::PhantomData<(*mut u8, core::marker::PhantomPinned)>,
+}
 
 
-// Same with above
 #[no_mangle]
 pub extern "C" fn hello() {
     println!("Hello world!");
 }
 
-
-
-
-
-
+// Stack allocated collections https://github.com/japaric/heapless
